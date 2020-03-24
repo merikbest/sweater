a27b6d164e0ac367e12c1e84e4ed534b6b39a15f
merikbest <merikbest2015@gmail.com>
2020-03-24 16:07:34 +0300

    done

--- src/main/resources/db/migration/V2__Add_admin.sql
+++ src/main/resources/db/migration/V2__Add_admin.sql
@@ -1,5 +1,5 @@
-insert into usr(id, username, password, active)
-    values (1, 'admin', '123', true );
+insert into usr(id, username, password, active, email)
+    values (1, 'admin', '$2a$08$IJIxzOQ3s5Ihxz9LlAQXx.R4OfwgVzrmM8Daj6zH3t5S0ULA9TtG6', true, 'merikbest2015@gmail.com');
 
 insert into user_role (user_id, roles)
     values (1, 'USER'), (1, 'ADMIN');
